cefdefd8666b57fd4ce51b7d8c98f20993bcd41d
Mohammed Shibili cv <121394653+mohd-shibilicv@users.noreply.github.com>
2024-05-04 12:23:24 +0530

    Add files via upload

---
+++ brohealth/settings.py
@@ -0,0 +1,265 @@
+import os
+import environ
+from pathlib import Path
+from datetime import timedelta
+from celery.schedules import crontab
+
+env = environ.Env(DEBUG=(bool, False))
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / "brohealth" / ".env")
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG")
+
+ALLOWED_HOSTS = ["main.d2jqgsuur54ac8.amplifyapp.com", "brohealth-backend.onrender.com", "127.0.0.1", "localhost", "djv9k9ys0eg02.cloudfront.net"]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "daphne",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
+    "rest_framework",
+    "drf_yasg",
+    "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
+    "django_celery_results",
+    "django_celery_beat",
+    "accounts.apps.AccountsConfig",
+    "patients.apps.PatientsConfig",
+    "doctors.apps.DoctorsConfig",
+    "admins.apps.AdminsConfig",
+    "appointments.apps.AppointmentsConfig",
+    "notifications.apps.NotificationsConfig",
+    "payments.apps.PaymentsConfig",
+    "prescriptions.apps.PrescriptionsConfig",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": ["rest_framework.filters.SearchFilter"],
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://localhost:8000",
+    "https://main.d2jqgsuur54ac8.amplifyapp.com",
+    "http://main.d2jqgsuur54ac8.amplifyapp.com",
+    "https://brohealth-backend.onrender.com",
+    "http://brohealth-backend.onrender.com",
+    "https://djv9k9ys0eg02.cloudfront.net",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://brohealth-backend.onrender.com",
+    "http://brohealth-backend.onrender.com",
+    "https://main.d2jqgsuur54ac8.amplifyapp.com",
+    "http://main.d2jqgsuur54ac8.amplifyapp.com",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "brohealth.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "brohealth.wsgi.application"
+
+ASGI_APPLICATION = "brohealth.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [env("DEFAULT_REDIS_URL", default=env("DEFAULT_REDIS_URL"))],
+        },
+    },
+}
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("PROD_DB_NAME"),
+        "USER": env("PROD_DB_USER"),
+        "PASSWORD": env("PROD_DB_PASS"),
+        "HOST": env("PROD_DB_HOST"),
+        "PORT": env("PROD_DB_PORT"),
+    }
+}
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASS"),
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),
+#     }
+# }
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Asia/Kolkata"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
+
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_COOKIE": "access_token",  # cookie name
+    "AUTH_COOKIE_DOMAIN": None,  # specifies domain for which the cookie will be sent
+    "AUTH_COOKIE_SECURE": False,  # restricts the transmission of the cookie to only occur over secure (HTTPS) connections.
+    "AUTH_COOKIE_HTTP_ONLY": True,  # prevents client-side js from accessing the cookie
+    "AUTH_COOKIE_PATH": "/",  # URL path where cookie will be sent
+    "AUTH_COOKIE_SAMESITE": "Lax",  # specifies whether the cookie should be sent in cross site requests
+}
+
+STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
+STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY")
+
+SITE_URL = env("FRONTEND_BASE_URL")
+
+CELERY_BROKER_URL = env("DEFAULT_REDIS_URL")
+CELERY_ACCEPT_CONTENT = {"application/json"}
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
+CELERY_TIMEZONE = "Asia/Kolkata"
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_CACHE_BACKEND = "django-cache"
+
+CELERY_BEAT_SCHEDULER = env("CELERY_BEAT_SCHEDULER")
+CELERY_BEAT_SCHEDULE = {
+    "send_appointment_reminder_emails": {
+        "task": "appointments.tasks.send_appointment_reminder_emails",
+        "schedule": crontab(minute="*/5"),
+    },
+    # 'remove_past_appointments': {
+    #     'task': 'appointments.tasks.remove_past_appointments',
+    #     'schedule': crontab(minute=0, hour=0),  # Run at midnight every day
+    # },
+}
+
+DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_QUERYSTRING_EXPIRE = 600
+
+AWS_S3_CUSTOM_DOMAIN = env("AWS_S3_CUSTOM_DOMAIN")
+
+AWS_CLOUDFRONT_KEY_ID = env.str("AWS_CLOUDFRONT_KEY_ID").strip()
+AWS_CLOUDFRONT_KEY = env.str("AWS_CLOUDFRONT_KEY", multiline=True).encode("ascii").strip()
---
+++ doctors/serializers.py
@@ -0,0 +1,96 @@
+import mimetypes
+from rest_framework import serializers
+
+from doctors.models import Doctor, Certificate, DoctorVerification, DoctorAvailability, FileTypeChoices
+from accounts.serializers import UserSerializer
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for the Doctor Model.
+    """
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Doctor
+        fields = (
+            'id',
+            'user',
+            'specialization',
+            'years_of_experience',
+            'education',
+            'clinic_address',
+            'clinic_phone_number',
+            'clinic_website',
+            'is_approved',
+        )
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = '__all__'
+
+
+class DoctorVerificationSerializer(serializers.ModelSerializer):
+    certificates = CertificateSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DoctorVerification
+        fields = [
+            'id',
+            'doctor',
+            'license_number',
+            'licensure_information',
+            'verification_status',
+            'certificates',
+        ]
+
+    def create(self, validated_data):
+        doctor_verification = DoctorVerification.objects.create(**validated_data)
+
+        index =  0
+        certificates_data = []
+        while True:
+            key = f'certificates[{index}]'
+            if key in self.context['request'].FILES:
+                file = self.context['request'].FILES[key]
+                file_type = self.get_file_type(file)
+                certificates_data.append({'file': file, 'type': file_type})
+                index +=  1
+            else:
+                break
+
+        # Create Certificate instances and add them to the doctor_verification instance
+        for certificate_data in certificates_data:
+            certificate = Certificate.objects.create(**certificate_data)
+            doctor_verification.certificates.add(certificate)
+
+        # Save the doctor_verification instance to update the ManyToManyField
+        doctor_verification.save()
+
+        return doctor_verification
+    
+    def get_file_type(self, file):
+        mime_type = mimetypes.guess_type(file.name)[0]
+        if mime_type and mime_type.startswith('image'):
+            return FileTypeChoices.IMAGE
+        elif mime_type and mime_type.startswith('application/pdf'):
+            return FileTypeChoices.PDF
+        else:
+            return Certificate.FileTypeChoices.IMAGE
+
+
+class DoctorAvailabilitySerializer(serializers.ModelSerializer):
+    """
+    A Serializer for the DoctorAvailability Model.
+    """
+    doctor = serializers.PrimaryKeyRelatedField(queryset=Doctor.objects.filter(is_approved=True))
+
+    class Meta:
+        model = DoctorAvailability
+        fields = [
+            'id',
+            'doctor',
+            'availability_schedule',
+        ]
